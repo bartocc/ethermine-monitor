fe0361e67de14cf6a741266db235c96840a8563e
Julien Palmas <julien.palmas@gmail.com>
2017-11-22 15:08:17 +0100

    workers

--- bin/monitor.rb
+++ bin/monitor.rb
@@ -9,17 +9,41 @@ WORKERS_COUNT = 2
 MINER = '1dE373F9C4C89FC061eA7a2a88d34721A83965a4'.freeze
 
 class EthminerClient
-  HOST = "api.ethermine.org"
+  SCHEME = 'https'
+  HOST = 'api.ethermine.org'.freeze
+
+  def self.url(path)
+    "#{SCHEME}://#{HOST}#{path}"
+  end
+
+  def initialize(miner)
+    @miner = miner
+  end
+
+  def workers_json
+    path = "/miner/#{@miner}/workers"
+    uri = URI self.class.url(path)
+    JSON.parse Net::HTTP.get(uri)
+  end
+
+  def workers
+    workers_json['data'].map do |hash|
+      Worker.new hash
+    end
+  end
+
+  def workers_count
+    workers.size
+  end
+
+  def worker_names
+    workers.map(&:worker)
+  end
 end
 
-url = "https://api.ethermine.org/miner/#{MINER}/workers"
-uri = URI url
-response = Net::HTTP.get uri
-json = JSON.parse response
-
-if json['data'].size == WORKERS_COUNT
-  puts "#{WORKERS_COUNT} workers found"
-  return
+class Worker < OpenStruct
 end
 
-raise 'do something'
+client = EthminerClient.new(MINER)
+puts client.workers_count
+puts client.worker_names
